d96da5dea688cfeb0f6f0fb6d4dbc8486c0c6c7f
Sijiw <1664479893@qq.com>
2023-04-08 18:41:06 +0800

    add switch component

--- packages/components/Switch/src/switch.ts
+++ packages/components/Switch/src/switch.ts
@@ -1,5 +1,10 @@
 import { ExtractPropTypes } from 'vue'
 
-export const switchProps = {}
+export const switchProps = {
+  modelValue: {
+    type: Boolean,
+    required: true
+  }
+}
 
 export type SwitchProps = ExtractPropTypes<typeof switchProps>
--- packages/components/Switch/src/switch.vue
+++ packages/components/Switch/src/switch.vue
@@ -1,22 +1,49 @@
 <template>
-  <div class="c-switch c-switch--large" @click="handleClick">
-    <span>left</span>
-    <div class="c-switch-container">
-      <div class="c-switch__inner"></div>
+  <div :class="classes" class="c-switch--large" @click="handleClick">
+    <span v-if="$slots.prefix" :class="prefixClasses">
+      <slot name="prefix"></slot>
+    </span>
+    <div :class="containerClasses">
+      <div :class="innerClasses"></div>
     </div>
+    <span v-if="$slots.suffix" :class="suffixClasses">
+      <slot name="suffix"> </slot>
+    </span>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from 'vue'
 import { switchProps } from './switch'
 import { useNamespace } from 'packages/hooks/use-namespace'
 
 const props = defineProps(switchProps)
-const emits = defineEmits(['click'])
+const emits = defineEmits(['click', 'update:modelValue'])
 const ns = useNamespace('switch')
 
+const switchState = ref(false)
+
+const classes = computed(() => {
+  const res = [ns.baseName]
+
+  return res
+})
+const containerClasses = computed(() => {
+  const res = [ns.addBlock('container')]
+
+  if (switchState.value) {
+    res.push(ns.addModifier('on'))
+  }
+
+  return res
+})
+const innerClasses = [ns.addElement('inner')]
+const prefixClasses = [ns.addBlock('prefix')]
+const suffixClasses = [ns.addBlock('suffix')]
+
 const handleClick = () => {
-  console.log('click')
+  switchState.value = !switchState.value
+  emits('update:modelValue', switchState.value)
   emits('click')
 }
 </script>
