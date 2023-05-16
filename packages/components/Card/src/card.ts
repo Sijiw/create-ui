f683334d6d670df130d622d9f76f524c4528c6df
Sijiw <1664479893@qq.com>
2023-05-16 14:22:01 +0800

    card component

--- packages/components/Card/src/card.ts
+++ packages/components/Card/src/card.ts
@@ -1,6 +1,16 @@
-import { ExtractPropTypes } from 'vue'
+import { CSSProperties, ExtractPropTypes, PropType } from 'vue'
 
 export type CardProps = ExtractPropTypes<typeof cardProps>
 export const cardProps = {
-  
+  shadow: {
+    type: String,
+    values: ['always', 'hover', 'never'],
+    default: 'always'
+  },
+  bodyStyle: {
+    type: Object as PropType<CSSProperties>
+  },
+  clickable: {
+    type: Boolean
+  }
 } as const
--- packages/components/Card/src/card.vue
+++ packages/components/Card/src/card.vue
@@ -1,15 +1,42 @@
 <template>
-  <div :class="ns.baseName">
+  <div :class="cardClasses" :style="bodyStyle">
+    <div :class="ns.addBlock('header')" v-if="$slots.header">
+      <slot name="header" />
+    </div>
+    <div :class="ns.addBlock('body')">
+      <slot />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useNamespace } from '@create-ui/hooks'
 import { cardProps } from './card'
+import { computed } from 'vue'
 
 const ns = useNamespace('card')
 const props = defineProps(cardProps)
+
+const cardClasses = computed(() => {
+  const res = [ns.baseName]
+
+  const { shadow, clickable } = props
+  switch (shadow) {
+    case 'never':
+      res.push(ns.addModifier('shadow-never'))
+      break
+    case 'hover':
+      res.push(ns.addModifier('shadow-hover'))
+  }
+
+  if (clickable === true) {
+    res.push(ns.addModifier('clickable'))
+  }
+
+  return res
+})
 </script>
 
-<style scoped lang="less"> 
+<style scoped lang="less">
+@import 'packages/styles/card/card.less';
 </style>
