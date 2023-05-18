6de9ea3c6b2dc9d83c633bd339197edb90bca1a3
Sijiw <1664479893@qq.com>
2023-05-18 10:05:04 +0800

    link component

--- packages/components/Link/src/link.ts
+++ packages/components/Link/src/link.ts
@@ -2,5 +2,17 @@ import { ExtractPropTypes } from 'vue'
 
 export type LinkProps = ExtractPropTypes<typeof linkProps>
 export const linkProps = {
-  
+  type: {
+    type: String
+  },
+  href: {
+    type: String
+  },
+  target: {
+    type: String,
+    values: ['_self', '_blank', '_parent', '_top']
+  },
+  disabled: {
+    type: Boolean
+  }
 } as const
--- packages/components/Link/src/link.vue
+++ packages/components/Link/src/link.vue
@@ -1,15 +1,38 @@
 <template>
-  <div :class="ns.baseName">
-  </div>
+  <a :class="aClasses" :href="href" :target="target" @click="handleClick">
+    <slot />
+  </a>
 </template>
 
 <script lang="ts" setup>
 import { useNamespace } from '@create-ui/hooks'
 import { linkProps } from './link'
+import { computed } from 'vue'
 
 const ns = useNamespace('link')
 const props = defineProps(linkProps)
+const emits = defineEmits(['click'])
+
+const aClasses = computed(() => {
+  const { type, disabled } = props
+
+  const res = [ns.baseName, ns.getClass('disabled', disabled)]
+
+  if (type) {
+    res.push(ns.addModifier(type))
+  }
+
+  return res
+})
+
+const handleClick = (e: Event) => {
+  if (props.disabled) {
+    e.preventDefault()
+  }
+  emits('click')
+}
 </script>
 
-<style scoped lang="less"> 
+<style scoped lang="less">
+@import 'packages/styles/link/link.less';
 </style>
